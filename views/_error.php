1349a8022ba8e64c414731600c8fff56f53e5a49
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2020-12-21 10:50:32 +0100

    #7 - Permission management with Middelware + forbiddenException

---
+++ views/_error.php
@@ -0,0 +1,9 @@
+<?php
+/* User: nicolashalberstadt 
+* Date: 14/12/2020 
+* Time: 10:48
+*/
+/**@var $exception \Exception */
+?>
+
+<h3><?= $exception->getCode() ?> - <?= $exception->getMessage() ?></h3>
