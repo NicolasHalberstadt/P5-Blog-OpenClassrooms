b317d322cd40a448a1d3b18d8ea7920b54fdc14a
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2020-12-21 12:07:31 +0100

    #5 - Updating migration file

--- migrations.php
+++ migrations.php
@@ -4,6 +4,7 @@
 * Time: 09:08
 */
 
+use app\models\User;
 use nicolashalberstadt\phpmvc\Application;
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -11,6 +12,7 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $config = [
+    'userClass' => User::class,
     'db' => [
         'dsn' => $_ENV['DB_DSN'],
         'user' => $_ENV['DB_USER'],
