a96d4a43d2ce537b92e3ddac6e98c0819c09eb0d
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2020-12-21 10:47:33 +0100

    #8 - AuthController : login form creation + register form. Login template

---
+++ models/LoginForm.php
@@ -0,0 +1,53 @@
+<?php
+/* User: nicolashalberstadt 
+* Date: 18/12/2020 
+* Time: 10:29
+*/
+
+namespace app\models;
+
+use app\core\Application;
+use app\core\Model;
+
+/**
+ * Class LoginForm
+ *
+ * @author Nicolas Halberstadt <halberstadtnicolas@gmail.com>
+ * @package app\models
+ */
+class LoginForm extends Model
+{
+    public string $email = '';
+    public string $password = '';
+
+    public function rules(): array
+    {
+        return [
+            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
+            'password' => [self::RULE_REQUIRED]
+        ];
+    }
+
+    public function labels(): array
+    {
+        return [
+            'email' => 'Your email',
+            'password' => 'Your password'
+        ];
+    }
+
+    public function login()
+    {
+        $user = User::findOne(['email' => $this->email]);
+        if (!$user) {
+            $this->addError('email', 'User does not exist with this email address');
+            return false;
+        }
+        if (!password_verify($this->password, $user->password)) {
+            $this->addError('password', 'Password is incorrect');
+            return false;
+        }
+
+        return Application::$app->login($user);
+    }
+}
