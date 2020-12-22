05b3f4d09d1a697ac750d01c506b3feaec04bb70
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2020-12-22 14:20:23 +0100

    #16 - MVC Application extension with isEditor method

---
+++ core/App.php
@@ -0,0 +1,30 @@
+<?php
+/* User: nicolashalberstadt 
+* Date: 22/12/2020 
+* Time: 14:02
+*/
+
+namespace app\core;
+
+use nicolashalberstadt\phpmvc\Application;
+
+/**
+ * Class App
+ *
+ * @author Nicolas Halberstadt <halberstadtnicolas@gmail.com>
+ * @package app\core
+ */
+class App extends Application
+{
+    public static function isEditor(): bool
+    {
+        if (Application::$app->user) {
+            $userType = Application::$app->user->user_type;
+            if ($userType == 2 || $userType == 3) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+}
