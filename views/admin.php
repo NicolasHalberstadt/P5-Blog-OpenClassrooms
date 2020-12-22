abb0e25174980f60e917807e3c99710f627e25dc
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2020-12-22 14:32:06 +0100

    #10 - Updating admin view depending on user status

--- views/admin.php
+++ views/admin.php
@@ -4,30 +4,30 @@
 * Time: 11:54
 */
 /**@var $users \app\models\User */
-
+/**@var $isAdmin bool */
 ?>
-<h1>Welcome to the admin panel</h1>
-
-
-<table class="table table-bordered">
-    <thead>
-    <tr>
-        <th scope="col">#</th>
-        <th scope="col">First</th>
-        <th scope="col">Last</th>
-        <th scope="col">Type</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php $index = 0;
-    foreach ($users as $user) : ?>
-        <?php $index++; ?>
+    <h1>Welcome to the admin panel</h1>
+<?php if ($isAdmin) : ?>
+    <table class="table table-bordered">Registered users
+        <thead>
         <tr>
-            <th scope="row"><?= $index ?></th>
-            <td><?= $user['firstname'] ?></td>
-            <td><?= $user['lastname'] ?></td>
-            <td><?= $user['user_type'] ?></td>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Type</th>
         </tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+        <?php $index = 0;
+        foreach ($users as $user) : ?>
+            <?php $index++; ?>
+            <tr>
+                <th scope="row"><?= $index ?></th>
+                <td><?= $user['firstname'] ?></td>
+                <td><?= $user['lastname'] ?></td>
+                <td><?= $user['user_type'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php endif; ?>
