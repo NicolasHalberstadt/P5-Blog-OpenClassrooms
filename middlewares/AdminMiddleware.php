4cdc420f88075ff3ddd08879ae53ea52d6e0d9bf
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2020-12-22 14:31:25 +0100

    #7 - Permission : AdminMiddleware created

---
+++ middlewares/AdminMiddleware.php
@@ -0,0 +1,41 @@
+<?php
+/* User: nicolashalberstadt 
+* Date: 22/12/2020 
+* Time: 11:49
+*/
+
+namespace app\middlewares;
+
+use app\core\App;
+use nicolashalberstadt\phpmvc\exceptions\ForbiddenException;
+use nicolashalberstadt\phpmvc\middlewares\BaseMiddleware;
+
+/**
+ * Class AdminMiddleware
+ *
+ * @author Nicolas Halberstadt <halberstadtnicolas@gmail.com>
+ * @package app\middlewares
+ */
+class AdminMiddleware extends BaseMiddleware
+{
+
+    public array $actions;
+
+    /**
+     * AdminMiddleware constructor.
+     * @param array $actions
+     */
+    public function __construct(array $actions = [])
+    {
+        $this->actions = $actions;
+    }
+
+    public function execute()
+    {
+        if (!App::isAdmin()) {
+            if (empty($this->actions) || in_array(App::$app->controller->action, $this->actions)) {
+                throw new ForbiddenException();
+            }
+        }
+    }
+}
