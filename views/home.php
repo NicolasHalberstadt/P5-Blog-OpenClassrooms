82002a269445844184e0a6f1478b3427d59ad89d
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2020-12-22 14:18:29 +0100

    #10 - Posts display front page

--- views/home.php
+++ views/home.php
@@ -2,7 +2,10 @@
 /** @var $this View */
 /** @var $model ContactForm */
 
+/** @var $posts Post */
+
 use app\models\ContactForm;
+use app\models\Post;
 use nicolashalberstadt\phpmvc\form\Form;
 use nicolashalberstadt\phpmvc\form\TextareaField;
 use nicolashalberstadt\phpmvc\View;
@@ -11,7 +14,23 @@ $this->title = 'Home'
 ?>
 <h1>Home</h1>
 
-
+<!-- Posts lists -->
+<h2>Posts list</h2>
+<?php foreach ($posts as $post) : ?>
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $post['title'] ?></h5>
+                    <p class="card-text"><?= $post['chapo'] ?></p>
+                    <a href="#" class="btn btn-primary">Read the post</a>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endforeach; ?>
+<!-- Posts lists -->
+<!-- Contact form -->
 <h3><a id="contact"></a>Contact</h3>
 
 <?php $form = Form::begin('', 'post') ?>
@@ -21,3 +40,4 @@ $this->title = 'Home'
 
 <button type="submit" class="btn btn-primary">Submit</button>
 <?php $form = Form::end(); ?>
+<!-- Contact form -->
