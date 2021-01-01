642bab683fd46390b8fdd66dd47a0cc0528f0c22
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-01-01 17:32:22 +0100

    #16 - Show post method

--- controllers/BlogController.php
+++ controllers/BlogController.php
@@ -27,6 +27,18 @@ class BlogController extends Controller
         $this->registerMiddleware(new EditorMiddleware(['addPost']));
     }
 
+    public function showPost(Request $request, Response $response)
+    {
+        $post = Post::findOne(['id' => $_GET['id']]);
+        if (!$post) {
+            Application::$app->session->setFlash('error', 'No post with this id exists in the database');
+            $response->redirect('/');
+        }
+        return $this->render('post', [
+            'post' => $post
+        ]);
+    }
+
     public function addPost(Request $request, Response $response)
     {
         $post = new Post();
@@ -41,6 +53,28 @@ class BlogController extends Controller
             ]);
         }
         return $this->render('add_post', [
+            'model' => $post
+        ]);
+    }
+
+    public function editPost(Request $request, Response $response)
+    {
+        $postId = $_GET['id'];
+        $post = Post::findOne(['id' => $postId]);
+        if (!$post) {
+            Application::$app->session->setFlash('error', 'No post with this id exists in the database');
+            $response->redirect('/admin');
+        }
+        if ($request->isPost()) {
+            $post->loadData($request->getBody());
+            if ($post->update()) {
+                Application::$app->session->setFlash('success', 'The post has been successfully updated');
+            }
+            return $this->render('edit_post', [
+                'model' => $post
+            ]);
+        }
+        return $this->render('edit_post', [
             'model' => $post
         ]);
     }
