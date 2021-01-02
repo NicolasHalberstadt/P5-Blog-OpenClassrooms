cf570996ece8785901bf3f1065b1eb42a1677d10
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-01-02 16:59:28 +0100

    #10 - Edit comment form template

---
+++ views/edit_comment.php
@@ -0,0 +1,19 @@
+<?php
+/* User: nicolashalberstadt 
+* Date: 30/12/2020 
+* Time: 16:42
+*/
+?>
+<?php
+
+use app\models\Post;
+use nicolashalberstadt\phpmvc\form\TextareaField;
+use nicolashalberstadt\phpmvc\form\Form;
+
+/**@var $model Post */
+
+$form = Form::begin('', 'post') ?>
+<?php echo new TextareaField($model, 'content') ?>
+<button type="submit" class="btn btn-primary">Save</button>
+
+<?php \nicolashalberstadt\phpmvc\form\Form::end() ?>
