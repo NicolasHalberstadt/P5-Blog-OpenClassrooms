2995119a5e9e20e8f127f6451f509f1730cf1639
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-01-02 10:41:31 +0100

    #10 - post view : updating post view by adding edit link when user has editor or admin role

--- views/post.php
+++ views/post.php
@@ -3,14 +3,20 @@
 * Date: 30/12/2020 
 * Time: 16:50
 */
+
 /**@var $post \app\models\Post */
 
+use app\core\App;
 use app\models\User;
-
 ?>
 
-<h1><?= $post->title ?></h1>
-<h3><?= $post->chapo ?></h3>
-<p><?= $post->content ?></p>
-<p class="small-text">Written the <?= $post->created_at ?></p>
-<p class="small-text">Written by <?= User::findOne(['id' => $post->user_id])->getDisplayName() ?></p>
+    <h1><?= $post->title ?></h1>
+    <h3><?= $post->chapo ?></h3>
+    <p><?= $post->content ?></p>
+    <p class="small-text">Written the <?= $post->created_at ?></p>
+    <p class="small-text">Written by <?= User::findOne(['id' => $post->user_id])->getDisplayName() ?></p>
+<?php if (App::isEditor()) : ?>
+    <div class="">
+        <a href="/post/edit?id=<?= $post->id ?>">Edit Post</a>
+    </div>
+<?php endif; ?>
