64237859db481ec742c4bc159534d1e0e1d28bb7
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-01-06 17:04:04 +0100

    #7 - Routes : - Blog index - Cv - Comment delete

--- public/index.php
+++ public/index.php
@@ -25,9 +25,10 @@ $config = [
 
 $app = new Application(dirname(__DIR__), $config);
 
-// Home
+// Site
 $app->router->get('/', [SiteController::class, 'home']);
 $app->router->post('/', [SiteController::class, 'home']);
+$app->router->get('/cv', [SiteController::class, 'cv']);
 
 // Admin
 $app->router->get('/admin', [SiteController::class, 'admin']);
@@ -36,16 +37,15 @@ $app->router->post('/user/edit', [SiteController::class, 'editUser']);
 $app->router->get('/user/delete', [SiteController::class, 'deleteUser']);
 $app->router->post('/user/delete', [SiteController::class, 'deleteUser']);
 
-
 // Auth
 $app->router->get('/login', [AuthController::class, 'login']);
 $app->router->post('/login', [AuthController::class, 'login']);
 $app->router->get('/register', [AuthController::class, 'register']);
 $app->router->post('/register', [AuthController::class, 'register']);
 $app->router->get('/logout', [AuthController::class, 'logout']);
-$app->router->get('/profile', [AuthController::class, 'profile']);
 
 // Blog
+$app->router->get('/blog', [BlogController::class, 'blog']);
 $app->router->get('/post', [BlogController::class, 'showPost']);
 $app->router->post('/post', [BlogController::class, 'showPost']);
 $app->router->get('/post/add', [BlogController::class, 'addPost']);
@@ -58,5 +58,7 @@ $app->router->get('/comment/validate', [BlogController::class, 'validateComment'
 $app->router->post('/comment/validate', [BlogController::class, 'validateComment']);
 $app->router->get('/comment/edit', [BlogController::class, 'editComment']);
 $app->router->post('/comment/edit', [BlogController::class, 'editComment']);
+$app->router->get('/comment/delete', [BlogController::class, 'deleteComment']);
+$app->router->post('/comment/delete', [BlogController::class, 'deleteComment']);
 
 $app->run();
