7240e13354687a03726fd6fe7d34d72fabc5437f
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-01-06 17:20:14 +0100

    #10 - Home page : removing posts display and refactoring contact form

--- views/home.php
+++ views/home.php
@@ -13,49 +13,28 @@ use nicolashalberstadt\phpmvc\View;
 
 $this->title = 'Home'
 ?>
-<h1>Home</h1>
 
-<!-- Posts lists -->
-<h2>Posts list</h2>
-<div class="row row-cols-1 row-cols-md-3 g-4">
-    <?php foreach ($posts as $post) : ?>
-        <div class="col">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $post['title'] ?></h5>
-                    <p class="card-text"><?= $post['chapo'] ?></p>
-                    <p class="card-text"><small
-                                class="text-muted">Last modified on <?php $t = strtotime($post['updated_at']);
-                            echo date('F jS Y', $t); ?></small>
-                    </p>
-                    <a href="post?id=<?= $post['id'] ?>" class="btn btn-primary">Read the post</a>
-                    <?php if (App::isEditor()): ?>
-                        <a href="post/edit?id=<?= $post['id'] ?>" class="btn btn-info">Edit the post</a>
-                        <a onClick="javascript: return confirm('Please confirm deletion');"
-                           href="post/delete?id=<?= $post['id'] ?>" class="btn btn-danger">Delete the post</a>
-                    <?php endif; ?>
-                </div>
+<!-- Contact form -->
+<div class="form-container container">
+    <div class="intro">
+        <h3 class="text-center"><a id="contact"></a>Contact</h3>
+    </div>
+    <div class="form">
+        <?php $form = Form::begin('', 'post') ?>
+        <div class="row">
+            <div class="col">
+                <?php echo $form->field($model, 'firstname') ?>
+            </div>
+            <div class="col">
+                <?php echo $form->field($model, 'lastname') ?>
             </div>
         </div>
-    <?php endforeach; ?>
-</div>
-<!-- Posts lists -->
-<!-- Contact form -->
-<h3><a id="contact"></a>Contact</h3>
+        <?php echo $form->field($model, 'email'); ?>
+        <?php echo $form->field($model, 'subject'); ?>
+        <?php echo new TextareaField($model, 'body') ?>
 
-<?php $form = Form::begin('', 'post') ?>
-<div class="row">
-    <div class="col">
-        <?php echo $form->field($model, 'firstname') ?>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        <?php $form = Form::end(); ?>
     </div>
-    <div class="col">
-        <?php echo $form->field($model, 'lastname') ?>
-    </div>
+    <!-- Contact form -->
 </div>
-<?php echo $form->field($model, 'email'); ?>
-<?php echo $form->field($model, 'subject'); ?>
-<?php echo new TextareaField($model, 'body') ?>
-
-<button type="submit" class="btn btn-primary">Submit</button>
-<?php $form = Form::end(); ?>
-<!-- Contact form -->
