fb728da1a772a22f9b5f3e6a55b451d7fdd6b725
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-01-06 17:09:36 +0100

    #10 - Changes : - Updating layout with theme - Changing header for admin

--- views/layouts/admin.php
+++ views/layouts/admin.php
@@ -1,7 +1,7 @@
 <?php
 /* User: nicolashalberstadt 
-* Date: 22/12/2020 
-* Time: 12:09
+* Date: 14/12/2020 
+* Time: 10:25
 */
 
 use nicolashalberstadt\phpmvc\Application;
@@ -13,57 +13,85 @@ use nicolashalberstadt\phpmvc\Application;
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/4b9ba14b0f.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css"/>
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet"
+          type="text/css"/>
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link rel="stylesheet" href="/css/theme.css">
     <!-- CSS -->
-    <link rel="stylesheet" href="../css/style.css">
-    <title><?= $this->title ?></title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="icon" href="/assets/img/favicon.ico">
+
+    <title><?= $this->title ?> | Nicolas Halberstadt - Symfony/PHP web developer</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/#contact">Contact</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/post/add">Add a post</a>
-            </li>
-        </ul>
-        <?php if (Application::isGuest()): ?>
+<nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand" href="/">
+            <img src="/assets/img/profilepic.jpg" alt="">
+        </a>
+        <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded"
+                type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive"
+                aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/login">Login <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/register">Register</a>
-                </li>
-            </ul>
-        <?php else: ?>
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="/profile">Profile</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="/logout">Welcome <?= Application::$app->user->getDisplayName() ?>
-                        (Logout)
+                <li class="nav-item mx-0 mx-lg-1">
+                    <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+                       href="/post/add">Add a post
+                        <i class="fas fa-pen-fancy"></i>
                     </a>
                 </li>
+                <li class="nav-item mx-0 mx-lg-1">
+                    <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+                       href="/#contact">Contact
+                        <i class="fas fa-envelope-open-text"></i>
+                    </a>
+                </li>
+                <li class="nav-item mx-0 mx-lg-1">
+                    <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+                       href="/blog">Blog
+                        <i class="far fa-newspaper"></i>
+                    </a>
+                </li>
+                <li class="nav-item mx-0 mx-lg-1">
+                    <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+                       href="/cv">CV
+                        <i class="fas fa-user-circle"></i>
+                    </a>
+                </li>
+                <?php if (Application::isGuest()): ?>
+                    <li class="nav-item mx-0 mx-lg-1">
+                        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+                           href="/login">Login</a>
+                    </li>
+                    <li class="nav-item mx-0 mx-lg-1">
+                        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+                           href="/register">Register</a>
+                    </li>
+                <?php else: ?>
+                    <li class="nav-item mx-0 mx-lg-1">
+                        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+                           href="/logout">Logout
+                            <i class="fas fa-sign-out-alt"></i>
+                        </a>
+                    </li>
+                <?php endif; ?>
             </ul>
-        <?php endif; ?>
+        </div>
     </div>
 </nav>
+
 <div class="container">
     <?php if (Application::$app->session->getFlash('error')): ?>
         <div class="alert alert-danger">
@@ -75,13 +103,65 @@ use nicolashalberstadt\phpmvc\Application;
             <?php echo Application::$app->session->getFlash('success') ?>
         </div>
     <?php endif; ?>
+    <header class="masthead bg-primary text-white text-center">
+        <div class="d-flex align-items-center flex-column">
+            <!-- Masthead Avatar Image-->
+            <!--<img class="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt=""/>-->
+            <!-- Masthead Heading-->
+            <h1 class="masthead-heading text-uppercase mb-0">Admin panel</h1>
+            <!-- Icon Divider-->
+            <div class="divider-custom divider-light">
+                <div class="divider-custom-line"></div>
+                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                <div class="divider-custom-line"></div>
+            </div>
+            <!-- Masthead Subheading-->
+            <p class="masthead-subheading font-weight-light mb-0">Hello <?= Application::$app->user->firstname ?>,
+                welcome to the admin panel</p>
+        </div>
+    </header>
     {{content}}
 </div>
-<nav class="navbar navbar-light bg-light">
-    <a class="navbar-brand" href="/admin">
-        Admin panel
-    </a>
-</nav>
+<footer class="footer text-center">
+    <div class="container">
+        <div class="row">
+            <!-- Footer Location-->
+            <div class="col-lg-3 mb-5 mb-lg-0">
+                <h4 class="text-uppercase mb-4">Location</h4>
+                <p class="lead mb-0">
+                    Aix-en-Provence, 13100
+                    <br/>
+                    France
+                </p>
+            </div>
+            <!-- Footer Social Icons-->
+            <div class="col-lg-3 mb-5 mb-lg-0">
+                <h4 class="text-uppercase mb-4">Around the Web</h4>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com/in/nicolashalberstadt/">
+                    <i class="fab fa-fw fa-linkedin-in"></i>
+                </a>
+                <a class="btn btn-outline-light btn-social mx-1" href="https://github.com/NicolasHalberstadt">
+                    <i class="fab fa-github"></i>
+                </a>
+            </div>
+            <!-- Footer About Text-->
+            <div class="col-lg-3">
+                <h4 class="text-uppercase mb-4">About Freelancer</h4>
+                <p class="lead mb-0">
+                    Freelance is a free to use, MIT licensed Bootstrap theme created by
+                    <a href="http://startbootstrap.com">Start Bootstrap</a>
+                    .
+                </p>
+            </div>
+            <div class="col-lg-3">
+                <h4 class="text-uppercase mb-4">Admin panel</h4>
+                <a class="btn btn-outline-light btn-social mx-1" href="/admin">
+                    <i class="fas fa-user-lock"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
 
 <!-- Optional JavaScript; choose one of the two! -->
 <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
