b1232a10901c06dc9656b0f7b94e7bec4a90e914
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-01-06 17:16:00 +0100

    #10 - Dynamic error page

--- views/_error.php
+++ views/_error.php
@@ -3,7 +3,38 @@
 * Date: 14/12/2020 
 * Time: 10:48
 */
-/**@var $exception \Exception */
+/**@var $exception Exception */
+$this->title = 'Error '. $exception->getCode();
 ?>
+<?php
+$code = $exception->getCode();
+$string = "" . $exception->getCode() . "";
+$error = str_split($string, 1);
+?>
+<div class="mainbox">
+    <div class="error-code">
+        <div class="err"><?= $error[0] ?></div>
+        <i class="far fa-question-circle fa-spin"></i>
+        <div class="err2"><?= $error[2] ?></div>
+    </div>
+    <div class="error-code-mobile err">
+        <p><?= $exception->getCode() ?></p>
+    </div>
+    <?php if ($code === 404) : ?>
+        <div class="msg">
+            Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first
+            place?
+            <p>Let's go <a href="/">home</a> and try from there.</p>
+        </div>
+    <?php elseif ($code === 403) : ?>
+        <div class="msg">Unfortunately you don't have the right to access this page...
+            <p>Let's go <a href="/">home</a> and try from there.</p>
+        </div>
+    <?php else : ?>
+        <div class="msg"><?= $exception->getMessage(); ?>
+            <p>Let's go <a href="/">home</a> and try from there.</p>
+        </div>
+    <?php endif; ?>
 
-<h3><?= $exception->getCode() ?> - <?= $exception->getMessage() ?></h3>
+</div>
+
