5feff7eaed40941db7fe6656ea057b02a886b022
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-01-06 17:19:28 +0100

    #10 - Allow Admin to delete user from edit-user page

--- views/edit_user.php
+++ views/edit_user.php
@@ -6,19 +6,23 @@
 /** @var $model \app\models\User */
 /** @var $userType string */
 /** @var $userStatus string */
-
+$this->title = 'Admin - edit user';
 ?>
-<h1>Edit <?= $model->getDisplayName() ?>'s account</h1>
-<?php $form = nicolashalberstadt\phpmvc\form\Form::begin('', 'post') ?>
+<div class="form-container">
+    <h3 class="text-center">Edit <?= $model->getDisplayName() ?>'s account</h3>
+    <div class="form">
+        <?php $form = nicolashalberstadt\phpmvc\form\Form::begin('', 'post') ?>
 
-<?php echo $form->field($model, 'firstname') ?>
-<?php echo $form->field($model, 'lastname') ?>
-<?php echo $form->field($model, 'email') ?>
-<?php echo new \app\core\form\UserTypeSelectInputField($model, 'type'); ?>
-<?php echo new \app\core\form\UserStatusSelectInputField($model, 'status'); ?>
-<button type="submit" class="btn btn-primary">Save</button>
+        <?php echo $form->field($model, 'firstname') ?>
+        <?php echo $form->field($model, 'lastname') ?>
+        <?php echo $form->field($model, 'email') ?>
+        <?php echo new \app\core\form\UserTypeSelectInputField($model, 'type'); ?>
+        <?php echo new \app\core\form\UserStatusSelectInputField($model, 'status'); ?>
+        <button type="submit" class="btn btn-primary">Save</button>
+        <a onClick="javascript: return confirm('Please confirm user deletion');"
+           href="/user/delete?id=<?= $model->id ?>"
+           class="btn btn-danger">Delete user</a>
+        <?php \nicolashalberstadt\phpmvc\form\Form::end() ?>
 
-<?php \nicolashalberstadt\phpmvc\form\Form::end() ?>
-
-<a onClick="javascript: return confirm('Please confirm user deletion');" href="/user/delete?id=<?= $model->id ?>"
-   class="btn btn-danger">Delete user</a>
+    </div>
+</div>
