44ed8c1d29a9a269502f00318d0db85fcc72a220
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-01-07 12:24:30 +0100

    #10 - Homepage : text presentation

--- views/home.php
+++ views/home.php
@@ -4,7 +4,6 @@
 
 /** @var $posts Post */
 
-use app\core\App;
 use app\models\ContactForm;
 use app\models\Post;
 use nicolashalberstadt\phpmvc\form\Form;
@@ -14,6 +13,31 @@ use nicolashalberstadt\phpmvc\View;
 $this->title = 'Home'
 ?>
 
+<div class="presentation-container">
+    <div class="presentation">
+        <h2>Who am I ?</h2>
+        <p>My name is Nicolas Halberstadt, I am 22 years old, I live in Aix-en-Provence and I am a web and web mobile
+            developer, specialized in Symfony.
+            I am passionate about new technologies, I like to discover new worlds. I love traveling, playing sports and
+            coding ... Of course!</p>
+        <h2>My journey</h2>
+        <p>After looking for my path for a few years after my baccalaureate and having always been attracted by the
+            digital
+            world, I started looking for training in this field. I chose the O'Clock school, Certified
+            <a href="https://www.grandeecolenumerique.fr">
+                "Grande École du Numérique"
+            </a>
+            , modern, dynamic and young: 5 intensive months to become a web developer, learning code in
+            (almost) every
+            way. 5 months to give us the tools to continue learning. Today, I'm studying PHP and Symfony through
+            OpenClassrooms'
+            course
+            <a href="https://openclassrooms.com/fr/paths/59-developpeur-dapplication-php-symfony">
+                "PHP/Symfony Application developer"
+            </a>.
+        </p>
+    </div>
+</div>
 <!-- Contact form -->
 <div class="form-container container">
     <div class="intro">
