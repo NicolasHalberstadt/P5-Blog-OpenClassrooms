945ea561d769d2e4ccd1b8d828ef63e9e577613c
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-01-07 12:34:48 +0100

    #10 - Edit post : new input field for author select in template

--- views/blog/edit_post.php
+++ views/blog/edit_post.php
@@ -6,6 +6,7 @@
 ?>
 <?php
 
+use app\core\form\PostUserSelectInputField;
 use app\models\Post;
 use nicolashalberstadt\phpmvc\form\TextareaField;
 use nicolashalberstadt\phpmvc\form\Form;
@@ -21,6 +22,7 @@ $this->title = 'Edit post';
         <?php echo $form->field($model, 'title') ?>
         <?php echo $form->field($model, 'chapo') ?>
         <?php echo new TextareaField($model, 'content') ?>
+        <?php echo new PostUserSelectInputField($model, 'user_id') ?>
         <button type="submit" class="btn btn-primary">Save</button>
 
         <?php \nicolashalberstadt\phpmvc\form\Form::end() ?>
