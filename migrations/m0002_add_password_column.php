20ba2767ce42f9816057a350ad28091e0dba6c3c
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2021-01-11 13:21:13 +0100

    #6 - Migrations : updating password length

--- migrations/m0002_add_password_column.php
+++ migrations/m0002_add_password_column.php
@@ -8,7 +8,7 @@ class m0002_add_password_column {
     public function up()
     {
         $db = \nicolashalberstadt\phpmvc\Application::$app->db;
-        $db->pdo->exec("ALTER TABLE users ADD COLUMN password VARCHAR(512) NOT NULL ");
+        $db->pdo->exec("ALTER TABLE users ADD COLUMN password VARCHAR(255) NOT NULL ");
     }
 
     public function down()
